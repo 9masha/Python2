4c06ef30b29062fe3f314fa25b48ca2ad1238996
9masha <82033199+9masha@users.noreply.github.com>
2021-08-03 20:45:52 +0200

    small refactoring but grid_forget still is not working

--- app_mitpaginator.py
+++ app_mitpaginator.py
@@ -7,8 +7,8 @@ window = Tk()
 window.title("Shopping Center <<Python 2>>")
 window.iconbitmap("images\icon.ico")
 frame = Frame(window)
-
 images = []
+
 
 def addToCart(productId):
     global order
@@ -46,56 +46,22 @@ def renderCatalog():
     global lPriceAmount
     global lPriceCurrency
     global page
+    global frame
+
     prf.saveAll(jds.getProducts())
     products = prf.all()
 
-    row = 1
-    page = 1
+
+    page = 0
     per_page = 5
     last_page = ceil(len(products) // per_page)
     frame.grid()
-
-    for product in products:
-        if 0 < product._id <= page * per_page + per_page :
-            img = ImageTk.PhotoImage(Image.open(product.image_path))
-            images.append(img)
-            image_label = Label(frame, image=img)
-            image_label.grid(row=row, column=0, columnspan=1)
-
-            lName = Label(frame, text=product.name, borderwidth=10)
-            lName.grid(row=row, column=1, sticky=W)
-
-            lPriceAmount = Label(frame, text=product.getPrice().amount)
-            lPriceAmount.grid(row=row, column=2)
-            lPriceCurrency = Label(frame, text=product.getPrice().currency, borderwidth=10)
-            lPriceCurrency.grid(row=row, column=3)
-
-            btnBuy = Button(frame, text="Buy", command=lambda _id=product._id: addToCart(_id))
-            btnBuy.grid(row=row, column=4)
-            row += 1
-            if row > per_page:
-                break
-
     status = Label(frame, text="Page 1 of " + str(last_page), bd=1, relief=SUNKEN, anchor=E)
 
-    def forward(image_number):
-        global my_label, product
-        global button_forward
-        global button_back
-        global image_label
-        global lName
-        global lPriceAmount
-        global lPriceCurrency
-        global page
-
-        frame.grid_forget()
-        frame.grid()
-        button_forward = Button(frame, text=">>", command=lambda: forward(image_number + 1))
-        button_back = Button(frame, text="<<", command=lambda: back(image_number - 1))
-
+    def productShow(page):
         row = 1
         for product in products:
-            if page * per_page < product._id <= page * per_page + per_page:
+            if page * per_page < product._id <= page * per_page + per_page :
                 img = ImageTk.PhotoImage(Image.open(product.image_path))
                 images.append(img)
                 image_label = Label(frame, image=img)
@@ -115,15 +81,32 @@ def renderCatalog():
                 if row > per_page:
                     break
 
+    productShow(page)
 
+    def forward(image_number):
+        global my_label, product
+        global button_forward
+        global button_back
+        global image_label
+        global lName
+        global lPriceAmount
+        global lPriceCurrency
+        global page
+        global frame
+
+        frame.grid_forget()
+        frame.grid()
+        button_forward = Button(frame, text=">>", command=lambda: forward(image_number + 1))
+        button_back = Button(frame, text="<<", command=lambda: back(image_number - 1))
         page += 1
-        if page == last_page:
+        productShow(page)
+
+        if page + 1  == last_page:
             button_forward = Button(frame, text=">>", state=DISABLED)
         button_back.grid(row=6, column=0)
         button_forward.grid(row=6, column=2)
 
-        status = Label(frame, text="Page " + str(page) + " of " + str(last_page), bd=1, relief=SUNKEN,
-                       anchor=E)
+        status = Label(frame, text="Page " + str(page+1) + " of " + str(last_page), bd=1, relief=SUNKEN, anchor=E)
         status.grid(row=7, column=0, columnspan=3, sticky=W + E)
 
     def back(image_number):
@@ -138,6 +121,7 @@ def renderCatalog():
         global lPriceAmount
         global lPriceCurrency
         global page
+        global frame
 
         frame.grid_forget()
         frame.grid()
@@ -146,36 +130,14 @@ def renderCatalog():
         button_forward = Button(frame, text=">>", command=lambda: forward(image_number + 1))
         button_back = Button(frame, text="<<", command=lambda: back(image_number - 1))
         page -= 1
-        row = 1
-        for product in products:
-            if (page-1 )* per_page < product._id <= (page -1) * per_page + per_page:
-                img = ImageTk.PhotoImage(Image.open(product.image_path))
-                images.append(img)
-                image_label = Label(frame, image=img)
-                image_label.grid(row=row, column=0, columnspan=1)
+        productShow(page)
 
-                lName = Label(frame, text=product.name, borderwidth=10)
-                lName.grid(row=row, column=1, sticky=W)
-
-                lPriceAmount = Label(frame, text=product.getPrice().amount)
-                lPriceAmount.grid(row=row, column=2)
-                lPriceCurrency = Label(frame, text=product.getPrice().currency, borderwidth=10)
-                lPriceCurrency.grid(row=row, column=3)
-
-                btnBuy = Button(frame, text="Buy", command=lambda _id=product._id: addToCart(_id))
-                btnBuy.grid(row=row, column=4)
-                row += 1
-                if row > per_page:
-                    break
-
-
-        if page == 1:
+        if page == 0:
             button_back = Button(frame, text="<<", state=DISABLED)
         button_back.grid(row=6, column=0)
         button_forward.grid(row=6, column=2)
 
-        status = Label(frame, text="Page " + str(page) + " of " + str(last_page), bd=1, relief=SUNKEN,
-                       anchor=E)
+        status = Label(frame, text="Page " + str(page+1) + " of " + str(last_page), bd=1, relief=SUNKEN, anchor=E)
         status.grid(row=7, column=0, columnspan=3, sticky=W + E)
 # ////
     button_back = Button(frame, text="<<", command=back, state=DISABLED)
@@ -196,15 +158,6 @@ def renderCart():
         print(thing)
     print(order)
 
-    # print(order)
-    # print(product)
-
-
-# btnCatalog = tk.Button(window, text="Catalog", command=renderCatalog)
-# btnCatalog.grid(row=0, column=0)
-#
-# btnCart = tk.Button(window, text="Cart", command=renderCart)
-# btnCart.grid(row=0, column=1)
 
 menubar = Menu(window)
 menubar.add_command(label="Catalog", command=renderCatalog)
